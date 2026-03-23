afd6a0aee7f34722678fc28ea5f82cbe816963fa
DamienCar06 <da.cardenas06@gmail.com>
2026-03-23 17:11:58 -0500

    Feature: add class Pawn into the package pieces AI: [Prompt] Asked for validation of moves for pawn piece ; [Adopt] parameters board and position; [Change] Pawn.possibleMoves

---
+++ main/java/pieces/Pawn.java
@@ -0,0 +1,51 @@
+package pieces;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Pawn extends Piece {
+    /**
+     * Constructs a new Pawn with the specified color.
+     * @param color the color of the pawn
+     */
+    public Pawn(Color color) {
+        super(PieceType.PAWN, color);
+    }
+
+    @Override
+    public List<Position> possibleMoves(Board board, Position pos) {
+        List<Position> moves = new ArrayList<>();
+        int dir = getColor() == Color.WHITE ? -1 : 1;
+        int startRow = getColor() == Color.WHITE ? 6 : 1;
+
+        // Forward moves
+        Position oneForward = new Position(pos.row + dir, pos.col);
+        if (Position.isValid(oneForward.row, oneForward.col) && board.getPiece(oneForward) == null) {
+            moves.add(oneForward);
+            // Two forward from start
+            if (pos.row == startRow) {
+                Position twoForward = new Position(pos.row + 2 * dir, pos.col);
+                if (Position.isValid(twoForward.row, twoForward.col) && board.getPiece(twoForward) == null) {
+                    Position mid = new Position(pos.row + dir, pos.col);
+                    if (board.getPiece(mid) == null) {
+                        moves.add(twoForward);
+                    }
+                }
+            }
+        }
+
+        // Capture pieces
+        for (int dc = -1; dc <= 1; dc += 2) {
+            Position capture = new Position(pos.row + dir, pos.col + dc);
+            if (Position.isValid(capture.row, capture.col)) {
+                Piece dest = board.getPiece(capture);
+                if (dest != null && dest.getColor() != getColor()) {
+                    moves.add(capture);
+                }
+            }
+        }
+
+        return moves;
+    }
+}
